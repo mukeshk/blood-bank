50e925cf59a98aa1fbfa53d8c893ec976061ed3c
Mukesh.Katariya <jmukesh@gmail.com>
2019-05-12 11:26:45 +0530

    Changed to PathParam and ListDTO

--- src/main/java/io/indorse/bloodbank/account/controller/AccountController.java
+++ src/main/java/io/indorse/bloodbank/account/controller/AccountController.java
@@ -5,6 +5,7 @@ import io.indorse.bloodbank.account.service.AccountService;
 import io.indorse.bloodbank.common.exception.RecordNotFoundException;
 import io.indorse.bloodbank.model.domain.BloodBankAccount;
 import io.indorse.bloodbank.model.dto.BloodBankAccountDTO;
+import io.indorse.bloodbank.model.dto.BloodBankAccountListDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +44,7 @@ public class AccountController {
      * @return Return the account detail
      */
     @GetMapping("/{uuid}")
-    public BloodBankAccountDTO findById(String uuid){
+    public BloodBankAccountDTO findById(@PathVariable("uuid") String uuid){
         BloodBankAccount account = accountService.findByUUID(uuid);
         if(account==null){
             throw new RecordNotFoundException("account record not found for uuid:="+ uuid);
@@ -58,11 +59,11 @@ public class AccountController {
      * @return returns the members of blood bank
      */
     @GetMapping("/")
-    public List<BloodBankAccountDTO> findAll(){
+    public List<BloodBankAccountListDTO> findAll(){
         Iterable<BloodBankAccount> accounts = accountService.findAll();
-        List<BloodBankAccountDTO> accountRecords = new ArrayList<>();
+        List<BloodBankAccountListDTO> accountRecords = new ArrayList<>();
         for (BloodBankAccount account:accounts) {
-            accountRecords.add(AccountMapper.map(account));
+            accountRecords.add(AccountMapper.mapToListDTO(account));
         }
         return accountRecords;
     }
